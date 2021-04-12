b26ad5805364959a1c25daf8fb1d6ef85bf7d602
Soumotanu Mazumdar <msoumotanu@gmail.com>
2021-04-12 17:25:20 +0530

    Electron React Bootstrap template

---
+++ src/renderer.js
@@ -0,0 +1,10 @@
+import "./scss/index.scss";
+import "bootstrap";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./js/App.js";
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
